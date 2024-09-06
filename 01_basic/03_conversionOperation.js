a60f66ffa07d99433b94cfa5b8ecdab63ca3bbfc
Pranav Harsh <pranavharshbara@gmail.com>
2024-09-06 21:44:38 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "44"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+//. different conversion types;--
+
+//  "33" => 33
+//"44abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "harsh" => true
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
