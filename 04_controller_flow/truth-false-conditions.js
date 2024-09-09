cd5bf808bc79c5b3dfbd486c6a30a7222d4eb802
Pranav Harsh <pranavharshbara@gmail.com>
2024-09-09 12:30:39 +0000

    truthy-falsy-conditions

---
+++ 04_controller_flow/truth-false-conditions.js
@@ -0,0 +1,43 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null, undefined
+
+let val1;
+// val1 = 5 ?? 10 // in case of using databases 
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20 // first value will be assigned here
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 900
+iceTeaPrice <= 1180 ? console.log("less than 1180") : console.log("more than 1180")
